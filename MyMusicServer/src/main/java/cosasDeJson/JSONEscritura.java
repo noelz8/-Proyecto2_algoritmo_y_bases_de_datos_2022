321183d98f3717d79e3978d4bf354c81c3cf6ea4
Ismael473 <62965318+Ismael473@users.noreply.github.com>
2022-05-27 21:59:01 -0600

    Solución de problemas en el creador de JSON y XML

--- MyMusicServer/src/main/java/cosasDeJson/JSONEscritura.java
+++ MyMusicServer/src/main/java/cosasDeJson/JSONEscritura.java
@@ -4,6 +4,8 @@
  */
 package cosasDeJson;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -18,39 +20,80 @@ public class JSONEscritura {
     }
 
     private static void crearJSON() {
-        /**
-         * 
-         * Esta funcion crea el archivo de formato JSON utilizando JSONObjects y JSONArray 
-         */
-        
-        JSONObject cancion = new JSONObject();
-        //cancion.put("cancion","Se pone una cancion con alguna funcion");
-        
-        JSONArray canciones = new JSONArray();
-        canciones.add(cancion);
-        
-        //Acá va un for o while para ingresar varios datos de distintos usuarios
-        JSONObject nombre = new JSONObject();
-        nombre.put("nombre","Acá viene una variable para que ingrese todos los datos");
-        JSONObject apellido = new JSONObject();
-        apellido.put("apellido", "Acá viene variable igual que la del nombre");
-        JSONObject edad = new JSONObject();
-        edad.put("edad", "Acá va int como variable para igresar todos los datos");
-        //Acá va un for o un while para crear varios JSONObjects, para los varios generos asignados a los usuarios
-        JSONObject genero = new JSONObject();
-        genero.put("genero", "rock,etc...(como variable para ingresar varios datos");
-        JSONArray generos = new JSONArray();
-        //Acá va un for o while para ingresar varios datos al JSONArray
-        generos.add(genero);
-        JSONObject contraseña = new JSONObject();
-        contraseña.put("contraseña", "Variable para contraseña");
-        //Acá va un for o un while para crear más amigos 
-        JSONObject amigo = new JSONObject();
-        amigo.put("amigo", "Nombre del amigo a modo de variable");
-        //Acá va un for o un while para ingresar la lista de amigos
+        // Se crean los generos
+        JSONObject generoRock = new JSONObject();
+        generoRock.put("genero","rock");
+        JSONObject generoJazz = new JSONObject();
+        generoJazz.put("genero","jazz");
+        JSONObject generoBlues = new JSONObject();
+        generoBlues.put("genero","blues");
+        //Los generos se agregan a una lista la cual los almacena como un arreglo de datos tipo JSON 
+        JSONArray generoList1 = new JSONArray();
+        generoList1.add(generoBlues);
+        generoList1.add(generoRock);
+        generoList1.add(generoJazz);
+        //Se crean los amigos
+        JSONObject amigo1 = new JSONObject();
+        amigo1.put("amigo1", "Noel");
+        JSONObject amigo2 = new JSONObject();
+        amigo2.put("amigo2","Christopher");
+        //Se agregan los datos de los amigos a una lista la cual los almacena como un arreglo de datos tipo JSON
         JSONArray amigos = new JSONArray();
-        amigos.add(amigos);
+        amigos.add(amigo1);
+        amigos.add(amigo2);
+        //Se crean los datos de la persona1
+        JSONObject persona1 = new JSONObject();
+        persona1.put("nombre", "Ismael");
+        persona1.put("apellido","Mena");
+        persona1.put("edad", 20);
+        persona1.put("genero",generoList1);
+        persona1.put("contraseña", "password");
+        persona1.put("amigos",amigos);
+        //Se crean otros generos musicales
+        JSONObject generoReggae = new JSONObject();
+        generoReggae.put("genero","reggae");
+        JSONObject generoSKA = new JSONObject();
+        generoSKA.put("genero","SKA");
+        JSONObject generoTecno = new JSONObject();
+        generoTecno.put("genero","tecno");
+        //Se almacenan dentro de un arreglo de datos tipo JSON
+        JSONArray generoList2 = new JSONArray();
+        generoList2.add(generoBlues);
+        generoList2.add(generoRock);
+        generoList2.add(generoJazz);
+        //Se crean los amigos de la segunda persona
+        JSONObject amigo3 = new JSONObject();
+        amigo1.put("amigo1", "Ismael");
+        JSONObject amigo4 = new JSONObject();
+        amigo2.put("amigo2","Noel");
+        //Se almacenan en un arreglo de datos tipo JSON
+        JSONArray amigos2 = new JSONArray();
+        amigos2.add(amigo3);
+        amigos2.add(amigo4);
+        //Se crea la segunda persona
+        JSONObject persona2 = new JSONObject();
+        persona2.put("nombre", "Christopher");
+        persona2.put("apellido","Blanco");
+        persona2.put("edad", 19);
+        persona2.put("genero",generoList2);
+        persona2.put("contraseña", "password");
+        persona2.put("amigos",amigos2);
+        //Se almacenan las personas en espacios separados para mejor legibilidad
+        JSONObject datosP1 = new JSONObject();
+        datosP1.put("datos", persona1);
+        JSONObject datosP2 = new JSONObject();
+        datosP2.put("datos", persona2);
+        //Se almacenan los espacios en un arreglo para que sea este el que se imprima en el documento
+        JSONArray listaPersonas = new JSONArray();
+        listaPersonas.add(datosP1);
+        listaPersonas.add(datosP2);
         
+        try(FileWriter file = new FileWriter("personas.json")){
+             file.write(listaPersonas.toJSONString());
+             file.flush();
+        }catch(IOException io){
+            io.printStackTrace();
+        }
         
-    }   
+    }
 }
--- MyMusicServer/src/main/java/cosasDeXML/XMLCreador.java
+++ MyMusicServer/src/main/java/cosasDeXML/XMLCreador.java
@@ -4,9 +4,20 @@
  */
 package cosasDeXML;
 
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -17,52 +28,70 @@ import org.w3c.dom.Text;
  * @author ismae
  */
 public class XMLCreador {
-    public static void main(String[] args) throws ParserConfigurationException {
-        creadorXML();
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+        try {
+            // TODO code application logic here
+            
+            DocumentBuilderFactory factory  = DocumentBuilderFactory.newInstance();
+            DocumentBuilder  builder = factory.newDocumentBuilder();
+            DOMImplementation implementation = builder.getDOMImplementation();
+            
+            Document documento = implementation.createDocument(null, "MyMusic", null);
+            documento.setXmlVersion("1.0");
+            
+            //Estos son los "tags" del XML 
+            
+            Element personas = documento.createElement("personas");
+            Element persona = documento.createElement("persona");
+            
+            Element nombre = documento.createElement("nombres");
+            Text textNombre = documento.createTextNode("Ismael");
+            nombre.appendChild(textNombre);
+            persona.appendChild(nombre);
+            
+            Element apellido = documento.createElement("apellido");
+            Text textApellido = documento.createTextNode("Mena");
+            apellido.appendChild(textApellido);
+            persona.appendChild(apellido);
+            
+            Element edad = documento.createElement("edad");
+            Text textEdad = documento.createTextNode("20");
+            edad.appendChild(textEdad);
+            persona.appendChild(edad);
+            
+            Element genero = documento.createElement("genero");
+            Text textGenero = documento.createTextNode("rock");
+            genero.appendChild(textGenero);
+            persona.appendChild(genero);            
+            
+            Element amigo = documento.createElement("amigo");
+            Text textAmigo = documento.createTextNode("Noel");
+            amigo.appendChild(textAmigo);
+            persona.appendChild(amigo);
+            
+            Element contraseña = documento.createElement("contraseña");
+            Text textContraseña = documento.createTextNode("password");
+            contraseña.appendChild(textContraseña);
+            persona.appendChild(contraseña);
+            
+            personas.appendChild(persona);
+            
+            documento.getDocumentElement().appendChild(personas);
+            
+            Source source = new DOMSource(documento);
+            Result result = new StreamResult(new File("MyMusic.xml"));
+            
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.transform(source, result);
+            
+        } catch (ParserConfigurationException | TransformerConfigurationException ex) {
+            Logger.getLogger(XMLCreador.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println(ex.getMessage());
+        }
+        
+        
+        
     }
 
-    private static void creadorXML() throws ParserConfigurationException {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        DOMImplementation implementation = builder.getDOMImplementation();
-        
-        Document documento = implementation.createDocument(null, "Usuarios", null);
-        documento.setXmlVersion("1.0");
-        
-        Element usuarios = documento.createElement("usuarios");
-        Element usuario = documento.createElement("usuario");
-        
-        Element nombre = documento.createElement("nombre");
-        Text textNombre = documento.createTextNode("Insertar variable para ingresar varios nombres");
-        nombre.appendChild(textNombre);
-        usuario.appendChild(nombre);
-        
-        Element apellido = documento.createElement("apellido");
-        Text textApellido = documento.createTextNode("Insertar varible para ingresar varios apellidos");
-        apellido.appendChild(textApellido);
-        usuario.appendChild(apellido);
-        
-        Element edad = documento.createElement("edad");
-        Text textEdad = documento.createTextNode("Insertar variable para ingresar varias edades");
-        edad.appendChild(textEdad);
-        usuario.appendChild(edad);
-        
-        Element genero = documento.createElement("genero");
-        Text textGenero = documento.createTextNode("Insertar variable con generos");
-        genero.appendChild(textGenero);
-        usuario.appendChild(genero);
-        
-        Element contraseña = documento.createElement("contraseña");
-        Text textContraseña = documento.createTextNode("Ingresar variable con la contraseña");
-        contraseña.appendChild(textContraseña);
-        usuario.appendChild(contraseña);
-        
-        Element amigo = documento.createElement("amigo");
-        Text textAmigo = documento.createTextNode("Ingresar variable con los amigos");
-        amigo.appendChild(textAmigo);
-        usuario.appendChild(amigo);
-        
-        usuarios.appendChild(usuario);
-    }
 }
 
